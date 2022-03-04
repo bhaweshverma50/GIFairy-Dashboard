c6cdf670c1d342a37e226597f6ac8e4b089bdc2a
Bhawesh <bhaweshverma50@gmail.com>
2022-03-04 12:55:27 +0530

    Get user guilds

--- discord-dashboard-api/src/controllers/guilds/index.ts
+++ discord-dashboard-api/src/controllers/guilds/index.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from "express";
-import { getBotGuildsService } from "../../services/guilds";
+import {
+  getBotGuildsService,
+  getUserGuildsService,
+} from "../../services/guilds";
+import { User } from "../../database/schemas/User";
 
 export async function getGuildsController(req: Request, res: Response) {
+  const user = req.user as User;
   try {
-    const { data } = await getBotGuildsService();
-    res.send({ status: true, data });
+    const { data: botGuilds } = await getBotGuildsService();
+    const { data: userGuilds } = await getUserGuildsService(user.id);
+    res.send({ status: true, data: { botGuilds, userGuilds } });
   } catch (error) {
     console.log(error);
     res.send({ status: false, error });
